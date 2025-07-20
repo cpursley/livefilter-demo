9264e1f5e2be725adbcac0640ba0632749cec2a2
Chase Pursley <chasepursley@gmail.com>
2025-07-20 16:31:13 -0400

    Fix livefilter naming and compilation

--- mix.exs
+++ mix.exs
@@ -53,12 +53,12 @@ defmodule TodoApp.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.1.1"},
+      {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       {:salad_ui, "~> 1.0.0-beta.3"},
       # Uncomment the line below to use a local version of live_filter
-      # {:live_filter, path: "../livefilter"},
-      {:live_filter, "~> 0.1.0", hex: :livefilter},
+      # {:livefilter, path: "../livefilter"},
+      {:livefilter, "~> 0.1.5"},
       {:tidewave, "~> 0.1", only: :dev},
       {:quantum, "~> 3.0"}
     ]
